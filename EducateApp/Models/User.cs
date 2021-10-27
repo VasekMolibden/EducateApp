eb086879e48850c5b065f24f6c9f1a1fa59b575c
A B <vasekmolibden@gmail.com>
2021-10-27 09:43:56 +0500

    Добавление регистрации

--- EducateApp/Models/User.cs
+++ EducateApp/Models/User.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 namespace EducateApp.Models
 {
     public class User : IdentityUser
